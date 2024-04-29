76644d8f6bb025bc5c4305b576e06d44bd81f378
aryj02 <126201000+aryj02@users.noreply.github.com>
2024-04-28 21:04:39 -0400

    Add files via upload
    Sam's code w/ fixed schema

--- finalproject-movies-schema.sql
+++ finalproject-movies-schema.sql
@@ -1,6 +1,3 @@
-//need to add FOREIGN KEYS, uniqueIds, check primary/reference keys & syntax
-//not sure if we're still keeping reviews, matchPercentage, watchLater, trendingList tables & need to add suggestedMovie tables?
-
 DROP TABLE account CASCADE CONSTRAINTS;
 CREATE TABLE account (
   fname             varchar2(15) not null, 
@@ -18,7 +15,7 @@ CREATE TABLE profile (
   accountId         char(9),
   profileId         char(9),                    
   pg13              char(1),
-  watchHistory      varchar2(15),      --change to boolean??
+  watchHistory      char(1),     
   primary key       (profileId),
   foreign key       (accountId) references account (userId)   
 );
@@ -28,76 +25,68 @@ CREATE TABLE movies (
   movieId           char(9) not null,
   title             varchar2(100) not null,
   movieYear         char(4),
-  directorId          char(9) not null,
+  directorId        char(9) not null,
   genre             varchar2(50) not null,
   runtime           char(6),                    --if wanna do in minutes?
-  summary           varchar2(200),
-  castId            char(9),
+  summary           varchar2(1000),
+  leadingActorId    char(9),
   productionCompany varchar2(50) not null,
   primary key (movieId),
-  foreign key (castId) references castInfo (actorId),
-  foreign key (director) references director (directorId),
-  foreign key (productionCompany) references productionCompany (productionName)
+  unique (leadingActorId),
+  unique (directorId),
+  unique (productionCompany)
 );
 
 DROP TABLE castInfo CASCADE CONSTRAINTS;
 CREATE TABLE castInfo (
+  actorId           char(9),
   actor_fname       varchar2(50) not null,
   actor_lname       varchar2(50) not null,
-  actorId           char(9),
   actor_bday        date,
-  movieId           char(9),
-  primary key       (actorId),
-  foreign key (movieId) references movies (movieID)
+  movieId           char(9) not null,
+  primary key       (actor_fname, actor_lname),
+  foreign key (movieId) references movies (movieId),
+  foreign key (actorId) references movies (leadingActorId)
 );
 
 DROP TABLE director CASCADE CONSTRAINTS;
 CREATE TABLE director (
+  directorId        char(9) not null,
   director_fname    varchar2(50) not null,
   director_lname    varchar2(50) not null,
-  directorId        char(9),
   director_bday     date,
   movieId           char(9),
   primary key       (directorId),
-  foreign key (movieId) references movies (movieID)
+  foreign key (movieId) references movies (movieID),
+  foreign key (directorId) references movies (directorId)
 );
 
---DROP TABLE productionCompany CASCADE CONSTRAINTS;
+DROP TABLE productionCompany CASCADE CONSTRAINTS;
 CREATE TABLE productionCompany (
   productionName    varchar(50) not null,
   movieId           char(9),
-  address           varchar2(150),
+  address           varchar2(100),
   primary key       (productionName), 
-  foreign key (movieId) references movies (movieId)
+  foreign key (movieId) references movies (movieId),
+  foreign key (productionName) references movies (productionCompany)
 );
 
 DROP TABLE reviews CASCADE CONSTRAINTS;
 CREATE TABLE reviews (
   reviewId          char(9),
   profileId         char(9),
-  starRating        char(5),
+  starRating        int,
   movieId           char(9),
   primary key       (reviewId), 
   foreign key (movieId) references movies (movieId),
   foreign key (profileId) references profile (profileId)
-  --foreign key (reviewId) references profile (accountId)?
 );  
 
-DROP TABLE watchLater CASCADE CONSTRAINTS;
+DROP TABLE watchlater CASCADE CONSTRAINTS;
 CREATE TABLE watchLater (
   movieId           char(9),
   profileId         char(9),
   primary key       (movieId, profileId),
   foreign key (movieId) references movies (movieId),
   foreign key (profileId) references profile (profileId)
-);
-
-DROP TABLE trendingList CASCADE CONSTRAINTS;
-CREATE TABLE trendingList (
-  movieId           char(9),
-  avgStarRating     char(5),        --percentage or 1-5 scale?
-  trendYear         char(4),       
-  genre             varchar(50),
-  primary key       (movieId, genre),
-  foreign key (movieId) references movies (movieId)
 );
